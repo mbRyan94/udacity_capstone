c237fc4e78a93fd04392da6943382ea0dd936115
Mike Schertl <schert@hm.edu>
2020-06-03 19:08:29 +0200

    add api test route

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, jsonify
 from models import setup_db
 from flask_cors import CORS
 from dotenv import load_dotenv
@@ -23,6 +23,10 @@ def create_app(test_config=None):
     @app.route('/coolkids')
     def be_cool():
         return "Be cool, man, be coooool! You're almost a FSND grad!"
+
+    @app.route('/api/test')
+    def api_test():
+        return jsonify({"msg": "API is working!"})
 
     return app
 
