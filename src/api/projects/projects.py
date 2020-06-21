bd51fbe7f34740808be16ddc1820da4adf2d2fdc
Mike Schertl <schert@hm.edu>
2020-06-21 22:27:48 +0200

    add project post route

--- src/api/projects/projects.py
+++ src/api/projects/projects.py
@@ -1,6 +1,7 @@
 from flask import request, abort, jsonify
 from flask_restful import Resource
 import sys
+import datetime
 from sqlalchemy.exc import SQLAlchemyError
 
 from src.db.models import Project
@@ -27,3 +28,32 @@ class Projects(Resource):
 
             })
         return jsonify(projects)
+
+    @require_auth('post:projects')
+    def post(self, jwt_payload):
+        try:
+            req_data = request.get_json()
+            print(req_data)
+            name = req_data['name']
+            description = req_data['description']
+            start_date = datetime.datetime.now()
+            end_date = start_date
+
+            new_project = Project(
+                name=name, description=description, start_date=start_date, end_date=end_date)
+            Project.insert(new_project)
+            projects = Project.query.all()
+            res = []
+            for project in projects:
+
+                res.append({
+                    "name": project.name,
+                    "description": project.description,
+                    "start_date": project.start_date,
+                    "end_date": project.end_date
+                })
+            # print(res)
+            return jsonify({"new_project": res})
+        except Exception:
+            print(sys.exc_info())
+            return jsonify({"new_person": "did not work"})
