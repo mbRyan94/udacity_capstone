ed694bd3ddc118e0eb5c9ea6d40038cb9aae966d
Mike Schertl <schert@hm.edu>
2020-06-24 19:39:28 +0200

    add profile route

---
+++ src/api/profile/profile.py
@@ -0,0 +1,33 @@
+from flask import abort, jsonify, request
+from flask_restful import Resource
+import sys
+import os
+import requests
+
+from src.authentication.auth import require_auth, AuthError, get_token_auth_header
+
+
+class Profile(Resource):
+    @require_auth('get:profile')
+    def get(self, jwt_payload):
+        try:
+            token = get_token_auth_header()
+            # print('token: ', token)
+            auth_domain = os.getenv('AUTH_DOMAIN')
+            req_url = f'{auth_domain}userinfo'
+            headers = {'Authorization': f'Bearer {token}'}
+            # print('headers: ', headers)
+            # print('req_url: ', req_url)
+            profile = requests.get(req_url, headers=headers)
+            # print('profile: ', profile)
+            if not profile:
+                raise AuthError({
+                    'status': 'unauthorized_fetch_profile',
+                    'description': 'fetching the profile failed'
+                }, 401)
+            json_profile = profile.json()
+
+            return {'profile': json_profile}
+        except requests.exceptions.RequestException:
+            print(sys.exc_info())
+            abort(400)
--- src/api/routes.py
+++ src/api/routes.py
@@ -1,6 +1,10 @@
 
 from src.api.projects.projects import Projects
+from src.api.projects.project import Project
+from src.api.profile.profile import Profile
 
 
 def initialize_routes(api):
     api.add_resource(Projects, '/api/projects')
+    api.add_resource(Project, '/api/projects/<int:project_id>')
+    api.add_resource(Profile, '/api/profile')
