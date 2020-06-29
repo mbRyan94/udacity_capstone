511401ba6a6df6ab3618a59abf79042ee8528bf8
Mike Schertl <schert@hm.edu>
2020-06-29 23:52:15 +0200

    add db queries for workspaces and workitems

--- src/db/query.py
+++ src/db/query.py
@@ -20,3 +20,23 @@ def get_all_projects_by_user_id(user_id):
 
 def get_project_by_id_and_user(user_id, project_id):
     return Project.query.filter(and_(user_id == Project.user_id, project_id == Project.id)).one_or_none()
+
+
+def get_all_workspaces_by_project_and_user(user_id, project_id):
+    return Workspace.query.filter(and_(project_id == Workspace.project_id, user_id == Project.user_id)).all()
+
+
+def get_workspace_by_id_project_id_and_user_id(user_id, project_id, workspace_id):
+    return Workspace.query.filter(and_(project_id == Workspace.project_id, user_id == Project.user_id, Workspace.id == workspace_id)).one_or_none()
+
+
+def check_project_ownership(user_id, project_id):
+    return Project.query.filter(and_(Project.user_id == user_id, Project.id == project_id)).one_or_none()
+
+
+def get_all_workitems_by_workspace_id(workspace_id):
+    return Workitem.query.filter(workspace_id == Workitem.workspace_id).all()
+
+
+def get_workspace_by_id(workspace_id):
+    return Workspace.query.filter(Workspace.id == workspace_id).one_or_none()
