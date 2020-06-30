16748236d27c76f7970523928fe4fe5f1e8e20ba
Mike Schertl <schert@hm.edu>
2020-06-30 08:39:21 +0200

    add new routes

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,12 +5,17 @@ from src.api.profile.profile import Profile
 from src.api.workitems.workitems import Workitems
 from src.api.workitems.workitem import Workitem
 from src.api.workspaces.workspaces import Workspaces
+from src.api.workspaces.workspace import Workspace
 
 
 def initialize_routes(api):
     api.add_resource(Projects, '/api/projects')
     api.add_resource(Project, '/api/projects/<int:project_id>')
-    api.add_resource(Workitems, '/api/workitems')
+
     api.add_resource(Workitem, '/api/workitems/<int:workitem_id>')
     api.add_resource(Profile, '/api/profile')
     api.add_resource(Workspaces, '/api/projects/<int:project_id>/workspaces')
+    api.add_resource(
+        Workspace, '/api/projects/<int:project_id>/workspaces/<int:workspace_id>')
+    api.add_resource(
+        Workitems, '/api//projects/<int:project_id>/workspaces/<int:workspace_id>/workitems')
