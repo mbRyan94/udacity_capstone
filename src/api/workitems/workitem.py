53cbd84d374c6eb3700662fdcb9e3287a897b82c
Mike Schertl <schert@hm.edu>
2020-06-30 09:41:01 +0200

    add workitem GET and PATCH routes

--- src/api/workitems/workitem.py
+++ src/api/workitems/workitem.py
@@ -1,22 +1,26 @@
-from flask import abort, request
+from flask import abort, jsonify, request
 from flask_restful import Resource
 import sys
-import os
-import requests
+import datetime
+from sqlalchemy.exc import SQLAlchemyError
 
-from src.authentication.auth import require_auth, AuthError
+from src.authentication.auth import require_auth, AuthError, get_token_user_id
 
-from src.db.models import Workitem
+from src.db.models import Workitem as db_Workitem
 import src.db.query as db
 
 
 class Workitem(Resource):
-    @require_auth('get:workitem')
-    def get(self, jwt_payload, workitem_id):
-        try:
+    method_decorators = [require_auth('get:workitem')]
 
+    def get(self, jwt_payload, project_id, workspace_id, workitem_id):
+        try:
+            user_id = get_token_user_id(jwt_payload)
+            print('work: ', db.get_workitem_by_id_project_id_user_id_and_workspace_id(
+                user_id, project_id, workspace_id, workitem_id))
             print('workitem: ', workitem_id)
-            workitem = db.get_workitem_by_id(workitem_id)
+            workitem = db.get_workitem_by_id_project_id_user_id_and_workspace_id(
+                user_id, project_id, workspace_id, workitem_id)
             if not workitem:
                 print(sys.exc_info())
                 return {
@@ -28,59 +32,49 @@ class Workitem(Resource):
                 'id': workitem.id,
                 'name': workitem.name,
                 'description': workitem.description,
-                'duration': workitem.duration
+                'duration': workitem.duration,
+                'workspace_id': workitem.workspace_id
             }
         except Exception:
             print(sys.exc_info())
             abort(500)
 
-    @require_auth('delete:workitem')
-    def delete(self, jwt_payload, workitem_id):
+    method_decorators = [require_auth('patch:workitem')]
+
+    def patch(self, jwt_payload, project_id, workspace_id, workitem_id):
         try:
-            print('workitem_id: ', workitem_id)
-            workitem = db.get_workitem_by_id(workitem_id)
-            print(workitem)
-            test = workitem.delete()
-            if not test:
+            req_data = request.get_json()
+            print('req_data: ', req_data)
+            # name = req_data['name']
+            # description = req_data['description']
+            # duration = req_data['duration']
+
+            user_id = get_token_user_id(jwt_payload)
+            print('work: ', db.get_workitem_by_id_project_id_user_id_and_workspace_id(
+                user_id, project_id, workspace_id, workitem_id))
+
+            workitem = db.get_workitem_by_id_project_id_user_id_and_workspace_id(
+                user_id, project_id, workspace_id, workitem_id)
+            if not workitem:
                 print(sys.exc_info())
-            print('test: ', test)
-            return {'msg': 'works'}
+                return {
+                    'success': False,
+                    'error': 404,
+                    'message': 'resource not found'
+                }
+            if 'name' in req_data:
+                workitem.name = req_data['name']
+            if 'description' in req_data:
+                workitem.description = req_data['description']
+            if 'duration' in req_data:
+                workitem.duration = req_data['duration']
+
+            workitem.update()
+            updated_workitem = db.get_workitem_by_id_project_id_user_id_and_workspace_id(
+                user_id, project_id, workspace_id, workitem_id)
+
+            return {'workitem': updated_workitem.format()}
 
         except Exception:
             print(sys.exc_info())
             abort(500)
-
-    # @require_auth('post:workitem')
-    # def post(self, jwt_payload):
-    #     try:
-    #         res_data = []
-    #         req_data = request.get_json()
-    #         print('req_data: ', req_data)
-
-    #         name = req_data['name']
-    #         description = req_data['description']
-    #         duration = req_data['duration']
-    #         workspace_id = req_data['workspace_id']
-
-    #         new_workitem = Workitem(
-    #             name=name, description=description, duration=duration, workspace_id=workspace_id)
-
-    #         if not (new_workitem):
-    #             print(sys.exc_info())
-    #             return {
-    #                 'success': False,
-    #                 'error': 400,
-    #                 'message': 'bad request'
-    #             }
-    #         workitem = Workitem.insert(new_workitem)
-    #         print('workitem: ', workitem)
-    #         return {'msg': 'works'}
-    #         # return {
-    #         #     'id': workitem.id,
-    #         #     'name': workitem.name,
-    #         #     'description': workitem.description,
-    #         #     'duration': workitem.duration
-    #         # }
-    #     except Exception:
-    #         print(sys.exc_info())
-    #         abort(500)
