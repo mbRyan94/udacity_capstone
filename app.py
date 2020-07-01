9071136b10eccf6e81551568808692e463fa2ef2
Mike Schertl <schert@hm.edu>
2020-07-01 23:43:33 +0200

    refactoring

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 import sys
 from urllib.error import HTTPError
 
-from src.db.models import setup_db, Project, Workspace, Workitem, User
+from src.db.models import setup_db, Project, Workspace, Workitem
 from src.authentication.auth import require_auth, AuthError
 from src.api.routes import initialize_routes
 
@@ -48,36 +48,36 @@ def create_app(test_config=None):
             print(sys.exc_info())
             abort(404)
 
-    @app.route('/api/projects', methods=['POST'])
-    @require_auth('post:projects')
-    def add_project(jwt_payload):
-        try:
-            req_data = request.get_json()
-            print(req_data)
-            name = req_data['name']
-            description = req_data['description']
-            start_date = datetime.datetime.now()
-            end_date = start_date
-            print('end_time: ', end_date)
-            print(req_data)
-            new_project = Project(
-                name=name, description=description, start_date=start_date, end_date=end_date)
-            Project.insert(new_project)
-            projects = Project.query.all()
-            res = []
-            for project in projects:
+    # @app.route('/api/projects', methods=['POST'])
+    # @require_auth('post:projects')
+    # def add_project(jwt_payload):
+    #     try:
+    #         req_data = request.get_json()
+    #         print(req_data)
+    #         name = req_data['name']
+    #         description = req_data['description']
+    #         start_date = datetime.datetime.now()
+    #         end_date = start_date
+    #         print('end_time: ', end_date)
+    #         print(req_data)
+    #         new_project = Project(
+    #             name=name, description=description, start_date=start_date, end_date=end_date)
+    #         Project.insert(new_project)
+    #         projects = Project.query.all()
+    #         res = []
+    #         for project in projects:
 
-                res.append({
-                    "name": project.name,
-                    "description": project.description,
-                    "start_date": project.start_date,
-                    "end_date": project.end_date
-                })
-            # print(res)
-            return jsonify({"new_project": res})
-        except Exception:
-            print(sys.exc_info())
-            return jsonify({"new_person": "did not work"})
+    #             res.append({
+    #                 "name": project.name,
+    #                 "description": project.description,
+    #                 "start_date": project.start_date,
+    #                 "end_date": project.end_date
+    #             })
+    #         # print(res)
+    #         return jsonify({"new_project": res})
+    #     except Exception:
+    #         print(sys.exc_info())
+    #         return jsonify({"new_person": "did not work"})
 
     # @app.route('/api/projects')
     # @require_auth('get:projects')
@@ -107,35 +107,35 @@ def create_app(test_config=None):
     #             'projects': projects
     #         })
 
-    @app.route('/api/workspaces', methods=['POST'])
-    def add_workspaces():
-        try:
-            req_data = request.get_json()
-            print(req_data)
-            name = req_data['name']
-            description = req_data['description']
-            price = req_data['price']
-            project_id = req_data['project_id']
-            print('project_id: ', project_id)
-            print(req_data)
-            new_workspace = Workspace(
-                name=name, description=description, price=price, project_id=project_id)
-            Workspace.insert(new_workspace)
-            workspaces = Workspace.query.all()
-            res = []
-            for workspace in workspaces:
+    # @app.route('/api/workspaces', methods=['POST'])
+    # def add_workspaces():
+    #     try:
+    #         req_data = request.get_json()
+    #         print(req_data)
+    #         name = req_data['name']
+    #         description = req_data['description']
+    #         price = req_data['price']
+    #         project_id = req_data['project_id']
+    #         print('project_id: ', project_id)
+    #         print(req_data)
+    #         new_workspace = Workspace(
+    #             name=name, description=description, price=price, project_id=project_id)
+    #         Workspace.insert(new_workspace)
+    #         workspaces = Workspace.query.all()
+    #         res = []
+    #         for workspace in workspaces:
 
-                res.append({
-                    "name": workspace.name,
-                    "description": workspace.description,
-                    "price": workspace.price,
-                    "project_id": workspace.project_id
-                })
-            # print(res)
-            return jsonify({"new_workspace": res})
-        except Exception:
-            print(sys.exc_info())
-            return jsonify({"new_workspace": "did not work"})
+    #             res.append({
+    #                 "name": workspace.name,
+    #                 "description": workspace.description,
+    #                 "price": workspace.price,
+    #                 "project_id": workspace.project_id
+    #             })
+    #         # print(res)
+    #         return jsonify({"new_workspace": res})
+    #     except Exception:
+    #         print(sys.exc_info())
+    #         return jsonify({"new_workspace": "did not work"})
 
     @app.route('/api/workitems', methods=['POST'])
     def add_workitem():
@@ -167,30 +167,30 @@ def create_app(test_config=None):
             print(sys.exc_info())
             return jsonify({"new_workspace": "did not work"})
 
-    @app.route('/api/users', methods=['POST'])
-    def add_user():
-        try:
-            req_data = request.get_json()
-            print(req_data)
-            name = req_data['name']
-            email = req_data['email']
+    # @app.route('/api/users', methods=['POST'])
+    # def add_user():
+    #     try:
+    #         req_data = request.get_json()
+    #         print(req_data)
+    #         name = req_data['name']
+    #         email = req_data['email']
 
-            new_user = User(
-                name=name, email=email)
-            User.insert(new_user)
-            users = User.query.all()
-            res_names = []
-            for user in users:
+    #         new_user = User(
+    #             name=name, email=email)
+    #         User.insert(new_user)
+    #         users = User.query.all()
+    #         res_names = []
+    #         for user in users:
 
-                res_names.append({
-                    "name": user.name,
-                    "email": user.email
-                })
-            # print(res)
-            return jsonify({"new_user": res_names})
-        except Exception:
-            print(sys.exc_info())
-            return jsonify({"new_user": "did not work"})
+    #             res_names.append({
+    #                 "name": user.name,
+    #                 "email": user.email
+    #             })
+    #         # print(res)
+    #         return jsonify({"new_user": res_names})
+    #     except Exception:
+    #         print(sys.exc_info())
+    #         return jsonify({"new_user": "did not work"})
 
     # Error Handling
 
