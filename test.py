975754672564632b9db69c24a1267020807f37c7
Mike Schertl <schert@hm.edu>
2020-07-02 10:03:56 +0200

    add post projects tests

--- test.py
+++ test.py
@@ -13,10 +13,12 @@ from flask.helpers import url_for
 from importlib.machinery import SourceFileLoader
 from app import create_app
 import src.db.models as db
+import src.db.query as query
+
 # app = SourceFileLoader("app", "app.py").load_module()
 # db = SourceFileLoader("module", "../db/module.py").load_module()
 # Please create a new token before running the tests
-token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ikhfb1J6RENzMTVoZXV4Ym9pWDd4SCJ9.eyJpc3MiOiJodHRwczovL2ZzbmQyMDIwLmV1LmF1dGgwLmNvbS8iLCJzdWIiOiJhdXRoMHw1ZWUzNDRlMDFmYjhjMzAwMTQ3MTQxM2MiLCJhdWQiOlsiZnJlZXRpbWUiLCJodHRwczovL2ZzbmQyMDIwLmV1LmF1dGgwLmNvbS91c2VyaW5mbyJdLCJpYXQiOjE1OTM2Njg3MjIsImV4cCI6MTU5MzY3NTkyMiwiYXpwIjoidk96YW9uanJmZmdQZGE3dUhOUTRxR3FySDRJdkV2aFUiLCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGVtYWlsIiwicGVybWlzc2lvbnMiOlsiZGVsZXRlOnByb2plY3QiLCJkZWxldGU6d29ya2l0ZW0iLCJnZXQ6cHJvZmlsZSIsImdldDpwcm9qZWN0IiwiZ2V0OnByb2plY3RzIiwiZ2V0OndvcmtpdGVtIiwiZ2V0OndvcmtpdGVtcyIsImdldDp3b3Jrc3BhY2UiLCJnZXQ6d29ya3NwYWNlcyIsIm9wZW5pZDplbWFpbCIsInBhdGNoOnByb2plY3QiLCJwYXRjaDp3b3JraXRlbSIsInBvc3Q6cHJvamVjdHMiLCJwb3N0OndvcmtpdGVtcyIsInBvc3Q6d29ya3NwYWNlcyJdfQ.RdFD2jzK3hd4nylQShRQ7ZnVpVw-Xw_tQwuMPZBn2_PuxNAS6E20kqxGKFhF-svc0MoDzqx7mWV4c8riklYirqjT_JwjyoUVW5ExXU0wwGp0XzDD6ZndI2J2sZf69PJCgEHex9BUjqZaY_xUAkepzF0X9qveqAWL1mQUachfbRJAaxufXiCBHWjNpNfTC9-RLp8JoCV6Aioqno8sFTSZS2_BBKXd_gCntdyC-JYrYsmZ8JjIUVeDkgO2I6_Ofnd81CblGmFEVZM3KeDRZT0II4sYqmPB0ubkgefgpSYao32ZYWAsqSfz8ZQPiVP8l03ECg2K5PLhurNzWdbxNZCA5A'
+token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ikhfb1J6RENzMTVoZXV4Ym9pWDd4SCJ9.eyJpc3MiOiJodHRwczovL2ZzbmQyMDIwLmV1LmF1dGgwLmNvbS8iLCJzdWIiOiJhdXRoMHw1ZWUzNDRlMDFmYjhjMzAwMTQ3MTQxM2MiLCJhdWQiOlsiZnJlZXRpbWUiLCJodHRwczovL2ZzbmQyMDIwLmV1LmF1dGgwLmNvbS91c2VyaW5mbyJdLCJpYXQiOjE1OTM2NzYxMDcsImV4cCI6MTU5MzY4MzMwNywiYXpwIjoidk96YW9uanJmZmdQZGE3dUhOUTRxR3FySDRJdkV2aFUiLCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGVtYWlsIiwicGVybWlzc2lvbnMiOlsiZGVsZXRlOnByb2plY3QiLCJkZWxldGU6d29ya2l0ZW0iLCJnZXQ6cHJvZmlsZSIsImdldDpwcm9qZWN0IiwiZ2V0OnByb2plY3RzIiwiZ2V0OndvcmtpdGVtIiwiZ2V0OndvcmtpdGVtcyIsImdldDp3b3Jrc3BhY2UiLCJnZXQ6d29ya3NwYWNlcyIsIm9wZW5pZDplbWFpbCIsInBhdGNoOnByb2plY3QiLCJwYXRjaDp3b3JraXRlbSIsInBvc3Q6cHJvamVjdHMiLCJwb3N0OndvcmtpdGVtcyIsInBvc3Q6d29ya3NwYWNlcyJdfQ.EMBjBLTb1YnR7cnlTuAg55A4WJzB13hDI3sju9hEJrGNbL6mqcymN9M1_28NOSqT3yyUIa42MZsrF_vzxrPTkzb4UZfokKPWAv2bVADeH-O6Noowjh1w9UPQINpbeZ1Hsy2-ecqTMsHPS8rULXMlGdXFPK21_EHyS6-rTppki6h3GkC_wDcE_QaDJguh9nFuivSLkwnzIykwpwZx8vLGXxr7zJ84SJlCTaKysSy6saOnU_HYrZ18-FeXot_CwJtZZQiBd8hJoqBHBhTB58OHbVycZerUjnrc6Vs1iG0t7c_H93xtrjr4gPeX9pDUWCxeVMdceDnDcZhXuayZduuIQg'
 
 # A token without openID scopes to fetch Auth0 /userinfo endpoint
 restricted_permission_token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ikhfb1J6RENzMTVoZXV4Ym9pWDd4SCJ9.eyJpc3MiOiJodHRwczovL2ZzbmQyMDIwLmV1LmF1dGgwLmNvbS8iLCJzdWIiOiJhdXRoMHw1ZWUzNDRlMDFmYjhjMzAwMTQ3MTQxM2MiLCJhdWQiOiJmcmVldGltZSIsImlhdCI6MTU5MzY3MDA1MiwiZXhwIjoxNTkzNjc3MjUyLCJhenAiOiJ2T3phb25qcmZmZ1BkYTd1SE5RNHFHcXJINEl2RXZoVSIsInNjb3BlIjoiIiwicGVybWlzc2lvbnMiOlsiZGVsZXRlOnByb2plY3QiLCJkZWxldGU6d29ya2l0ZW0iLCJnZXQ6cHJvZmlsZSIsImdldDpwcm9qZWN0IiwiZ2V0OnByb2plY3RzIiwiZ2V0OndvcmtpdGVtIiwiZ2V0OndvcmtpdGVtcyIsImdldDp3b3Jrc3BhY2UiLCJnZXQ6d29ya3NwYWNlcyIsIm9wZW5pZDplbWFpbCIsInBhdGNoOnByb2plY3QiLCJwYXRjaDp3b3JraXRlbSIsInBvc3Q6cHJvamVjdHMiLCJwb3N0OndvcmtpdGVtcyIsInBvc3Q6d29ya3NwYWNlcyJdfQ.JI6KJQClODrEb7Tiv-nFdkav_Ln7F9jfSidPc2GCEg3f7Md_atbxaZqhEjUkZG-gDb3ZetkyajJx67prl0dY5pfDskKSPAYWzekk5aokRIECCEvwsZpW7BUNRpPxeDapTzRUAS-rSE-hPccZspfTDLAV8R9M6wJv07PIOn1LtmU3CzQQAAWQzivGou5DPxiIkPdllO_BUYxaU5cB9t8SfJcYoAzofj5LxV-U9HAd_lMYHwmNAM0w7VZn8YAnsXEqStWxugHOS0fnYxar6yLq6GjX7nzh79KRgGgbnF-WGeFWL98eOtv9T0rLnf8qTE8Tr0a1w9_NwZvd-C-ebVFXZw'
@@ -70,13 +72,27 @@ class TestCases(unittest.TestCase):
         self.assertTrue(data['projects'])
 
     def test_get_projects_by_user_id_without_authorization_headers(self):
-        res = self.client().get('/api/projects', headers={'Authorization': 'Bearer {}'.format(token)})
+        res = self.client().get('/api/projects')
         data = json.loads(res.data)
         print('data: ', data)
         self.assertEqual(res.status_code, 401)
 
-    def test_post_new_project(self)
-        res = self.client().post('/api/projects', )
+    def test_post_new_project_success(self):
+        res = self.client().post('/api/projects', json=self.new_project , headers={'Authorization': 'Bearer {}'.format(token)})
+
+        data = json.loads(res.data)
+        print('post_new_project_data: ', data)
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(len(data['projects']) != 0)
+        self.assertTrue(data['success'])
+
+    def test_post_new_project_failure(self):
+        res = self.client().post('/api/projects/1', json=self.new_project , headers={'Authorization': 'Bearer {}'.format(token)})
+
+        data = json.loads(res.data)
+        print('post_new_project_data: ', data)
+        self.assertEqual(res.status_code, 405)
+        self.assertFalse(data['success'])
 
     # test profile route
     def test_profile(self):
@@ -94,8 +110,8 @@ class TestCases(unittest.TestCase):
         self.assertEqual(data['error'], 401)
         self.assertFalse(data['success'])
 
-    def test_get_user_and_project_specific_workspaces(self)
-        res = self.client().get()
+    # def test_get_user_and_project_specific_workspaces(self)
+    #     res = self.client().get()
         
 
 
