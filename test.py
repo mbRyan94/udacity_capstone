de7463dc010e94304f3b9826a76cdcd3d02eb97a
Mike Schertl <schert@hm.edu>
2020-07-02 09:28:48 +0200

    add new tests  + dummy post entries

--- test.py
+++ test.py
@@ -16,7 +16,11 @@ import src.db.models as db
 # app = SourceFileLoader("app", "app.py").load_module()
 # db = SourceFileLoader("module", "../db/module.py").load_module()
 # Please create a new token before running the tests
-token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ikhfb1J6RENzMTVoZXV4Ym9pWDd4SCJ9.eyJpc3MiOiJodHRwczovL2ZzbmQyMDIwLmV1LmF1dGgwLmNvbS8iLCJzdWIiOiJhdXRoMHw1ZWUzNDRlMDFmYjhjMzAwMTQ3MTQxM2MiLCJhdWQiOlsiZnJlZXRpbWUiLCJodHRwczovL2ZzbmQyMDIwLmV1LmF1dGgwLmNvbS91c2VyaW5mbyJdLCJpYXQiOjE1OTM2Mzk2MzUsImV4cCI6MTU5MzY0NjgzNSwiYXpwIjoidk96YW9uanJmZmdQZGE3dUhOUTRxR3FySDRJdkV2aFUiLCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGVtYWlsIiwicGVybWlzc2lvbnMiOlsiZGVsZXRlOnByb2plY3QiLCJkZWxldGU6d29ya2l0ZW0iLCJnZXQ6cHJvZmlsZSIsImdldDpwcm9qZWN0IiwiZ2V0OnByb2plY3RzIiwiZ2V0OndvcmtpdGVtIiwiZ2V0OndvcmtpdGVtcyIsImdldDp3b3Jrc3BhY2UiLCJnZXQ6d29ya3NwYWNlcyIsIm9wZW5pZDplbWFpbCIsInBhdGNoOnByb2plY3QiLCJwYXRjaDp3b3JraXRlbSIsInBvc3Q6cHJvamVjdHMiLCJwb3N0OndvcmtpdGVtcyIsInBvc3Q6d29ya3NwYWNlcyJdfQ.hKvg-7sUPHOe0uW5aoIH8XAp9aJzl8RgOloXZhX-ODMJJRyHfY4lLAfdtS5gOJbLiLXOPb1aD4kUTVkb3KBjZWAeLsJw3AZU6036p7fOtKYCGdQBjO6io-dnmGgxjqO1Uke7uImEea1cvTpfQPivaQ9KhexvxNyj-WRaRKaROIbgqZ86dhZ68B7Xrwgmul15ROV4ZYszKhuP7ndo9um3iSuxXM8dGwgCuiY2i5P-EFyIHp7fz_q3LkIplVl9CRhOoxVJZcCNDtncayGmQMWB8AC6OuLSFCscMLHxvjtJQcKOw2qwvgLno4Lv0pU8PV8CZ1Fv6-CF4ex1iBABDtd3uA'
+token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ikhfb1J6RENzMTVoZXV4Ym9pWDd4SCJ9.eyJpc3MiOiJodHRwczovL2ZzbmQyMDIwLmV1LmF1dGgwLmNvbS8iLCJzdWIiOiJhdXRoMHw1ZWUzNDRlMDFmYjhjMzAwMTQ3MTQxM2MiLCJhdWQiOlsiZnJlZXRpbWUiLCJodHRwczovL2ZzbmQyMDIwLmV1LmF1dGgwLmNvbS91c2VyaW5mbyJdLCJpYXQiOjE1OTM2Njg3MjIsImV4cCI6MTU5MzY3NTkyMiwiYXpwIjoidk96YW9uanJmZmdQZGE3dUhOUTRxR3FySDRJdkV2aFUiLCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGVtYWlsIiwicGVybWlzc2lvbnMiOlsiZGVsZXRlOnByb2plY3QiLCJkZWxldGU6d29ya2l0ZW0iLCJnZXQ6cHJvZmlsZSIsImdldDpwcm9qZWN0IiwiZ2V0OnByb2plY3RzIiwiZ2V0OndvcmtpdGVtIiwiZ2V0OndvcmtpdGVtcyIsImdldDp3b3Jrc3BhY2UiLCJnZXQ6d29ya3NwYWNlcyIsIm9wZW5pZDplbWFpbCIsInBhdGNoOnByb2plY3QiLCJwYXRjaDp3b3JraXRlbSIsInBvc3Q6cHJvamVjdHMiLCJwb3N0OndvcmtpdGVtcyIsInBvc3Q6d29ya3NwYWNlcyJdfQ.RdFD2jzK3hd4nylQShRQ7ZnVpVw-Xw_tQwuMPZBn2_PuxNAS6E20kqxGKFhF-svc0MoDzqx7mWV4c8riklYirqjT_JwjyoUVW5ExXU0wwGp0XzDD6ZndI2J2sZf69PJCgEHex9BUjqZaY_xUAkepzF0X9qveqAWL1mQUachfbRJAaxufXiCBHWjNpNfTC9-RLp8JoCV6Aioqno8sFTSZS2_BBKXd_gCntdyC-JYrYsmZ8JjIUVeDkgO2I6_Ofnd81CblGmFEVZM3KeDRZT0II4sYqmPB0ubkgefgpSYao32ZYWAsqSfz8ZQPiVP8l03ECg2K5PLhurNzWdbxNZCA5A'
+
+# A token without openID scopes to fetch Auth0 /userinfo endpoint
+restricted_permission_token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ikhfb1J6RENzMTVoZXV4Ym9pWDd4SCJ9.eyJpc3MiOiJodHRwczovL2ZzbmQyMDIwLmV1LmF1dGgwLmNvbS8iLCJzdWIiOiJhdXRoMHw1ZWUzNDRlMDFmYjhjMzAwMTQ3MTQxM2MiLCJhdWQiOiJmcmVldGltZSIsImlhdCI6MTU5MzY3MDA1MiwiZXhwIjoxNTkzNjc3MjUyLCJhenAiOiJ2T3phb25qcmZmZ1BkYTd1SE5RNHFHcXJINEl2RXZoVSIsInNjb3BlIjoiIiwicGVybWlzc2lvbnMiOlsiZGVsZXRlOnByb2plY3QiLCJkZWxldGU6d29ya2l0ZW0iLCJnZXQ6cHJvZmlsZSIsImdldDpwcm9qZWN0IiwiZ2V0OnByb2plY3RzIiwiZ2V0OndvcmtpdGVtIiwiZ2V0OndvcmtpdGVtcyIsImdldDp3b3Jrc3BhY2UiLCJnZXQ6d29ya3NwYWNlcyIsIm9wZW5pZDplbWFpbCIsInBhdGNoOnByb2plY3QiLCJwYXRjaDp3b3JraXRlbSIsInBvc3Q6cHJvamVjdHMiLCJwb3N0OndvcmtpdGVtcyIsInBvc3Q6d29ya3NwYWNlcyJdfQ.JI6KJQClODrEb7Tiv-nFdkav_Ln7F9jfSidPc2GCEg3f7Md_atbxaZqhEjUkZG-gDb3ZetkyajJx67prl0dY5pfDskKSPAYWzekk5aokRIECCEvwsZpW7BUNRpPxeDapTzRUAS-rSE-hPccZspfTDLAV8R9M6wJv07PIOn1LtmU3CzQQAAWQzivGou5DPxiIkPdllO_BUYxaU5cB9t8SfJcYoAzofj5LxV-U9HAd_lMYHwmNAM0w7VZn8YAnsXEqStWxugHOS0fnYxar6yLq6GjX7nzh79KRgGgbnF-WGeFWL98eOtv9T0rLnf8qTE8Tr0a1w9_NwZvd-C-ebVFXZw'
+
 
 
 class TestCases(unittest.TestCase):
@@ -39,23 +43,59 @@ class TestCases(unittest.TestCase):
             print(sys.exc_info())
 
         self.new_project = {
-            "name": "Project #1",
-            "description": "Client #1 - Biz and Marketing",
+            "name": "TestProject ",
+            "description": "Test Client - Biz and Marketing",
             "user_id": "5ee344e01fb8c3001471413c"
         }
 
+        self.new_workspace = {
+            "name": "Marketing",
+            "description": "Marketing stuff",
+            "price": 100
+        }
+
+        self.new_workitem = {
+            "name": "Social Media Campaign",
+            "description": "creating leads campaign",
+            "duration": 20
+        }
+
+    # test projects routes
     def test_get_projects_by_user_id(self):
         res = self.client().get('/api/projects', headers={'Authorization': 'Bearer {}'.format(token)})
         data = json.loads(res.data)
         print('data: ', data)
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data['success'], True)
+        self.assertTrue(data['projects'])
 
     def test_get_projects_by_user_id_without_authorization_headers(self):
-        res = self.client().get('/api/projects')
+        res = self.client().get('/api/projects', headers={'Authorization': 'Bearer {}'.format(token)})
         data = json.loads(res.data)
         print('data: ', data)
         self.assertEqual(res.status_code, 401)
+
+    def test_post_new_project(self)
+        res = self.client().post('/api/projects', )
+
+    # test profile route
+    def test_profile(self):
+        res = self.client().get('/api/profile', headers={'Authorization': 'Bearer {}'.format(token)})
+        data = json.loads(res.data)
+        print('data: ', data)
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(data['profile'])
+
+    def test_profile_without_role_permission(self):
+        res = self.client().get('/api/profile', headers={'Authorization': 'Bearer {}'.format(restricted_permission_token)})
+        data = json.loads(res.data)
+        print('data: ', data)
+        self.assertEqual(res.status_code, 401)
+        self.assertEqual(data['error'], 401)
+        self.assertFalse(data['success'])
+
+    def test_get_user_and_project_specific_workspaces(self)
+        res = self.client().get()
         
 
 
