bf8c0ed1eb2ff485e665c58baa7e6cd23d633491
Mike Schertl <schert@hm.edu>
2020-07-03 23:44:00 +0200

    refactoring

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ from dotenv import load_dotenv
 import sys
 from urllib.error import HTTPError
 
-from src.db.models import setup_db, Project, Workspace, Workitem
-from src.authentication.auth import require_auth, AuthError
+from src.db.models import setup_db
+from src.authentication.auth import AuthError
 from src.api.routes import initialize_routes
 import src.error_handlers.error_handlers as errors
 
@@ -30,55 +30,10 @@ def create_app(test_config=None):
     @app.route('/')
     def get_greeting():
         excited = os.getenv('EXCITED')
-        greeting = "Hello"
+        greeting = "My Capstone Project"
         if excited == 'true':
             greeting = greeting + "!!!!!"
         return greeting
-
-    @app.route('/coolkids')
-    def be_cool():
-        return "Be cool, man, be coooool! You're almost a FSND grad!"
-
-    @app.route('/api/test')
-    @require_auth('get:workspaces')
-    def api_test(jwt_payload):
-        try:
-            return jsonify({"msg": jwt_payload})
-
-        except Exception:
-            print(sys.exc_info())
-            abort(404)
-
-    @app.route('/api/workitems', methods=['POST'])
-    def add_workitem():
-        try:
-            req_data = request.get_json()
-            print(req_data)
-            name = req_data['name']
-            description = req_data['description']
-            duration = req_data['duration']
-            workspace_id = req_data['workspace_id']
-            print('workspace_id: ', workspace_id)
-            print(req_data)
-            new_item = Workitem(
-                name=name, description=description,
-                duration=duration, workspace_id=workspace_id)
-            Workitem.insert(new_item)
-            workitems = Workitem.query.all()
-            res = []
-            for workitem in workitems:
-
-                res.append({
-                    "name": workitem.name,
-                    "description": workitem.description,
-                    "duration": workitem.duration,
-                    "project_id": workitem.workspace_id
-                })
-            # print(res)
-            return jsonify({"new_workspace": res})
-        except Exception:
-            print(sys.exc_info())
-            return jsonify({"new_workspace": "did not work"})
 
     # Error Handling
 
