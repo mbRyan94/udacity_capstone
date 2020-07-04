be7eaa961cbdf7f1ed9efb189ade66426721dfd2
Mike Schertl <schert@hm.edu>
2020-07-04 10:24:23 +0200

    add interpreter files

---
+++ src/error_handlers/error_handlers.py
@@ -0,0 +1,10 @@
+# errors = {
+#     'ResourceNotFoundError': {
+#         'status': 404,
+#         'message': 'resource not found'
+#     },
+#     'Unauthorized': {
+#         'status': 401,
+#         'message': 'unauthorized bitch'
+#     }
+# }
