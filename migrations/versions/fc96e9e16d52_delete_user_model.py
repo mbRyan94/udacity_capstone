0d169e66ed75bbd7ee6bf4e7589a07a380399bcb
Mike Schertl <schert@hm.edu>
2020-07-05 18:00:40 +0200

    fix db migration file

--- migrations/versions/fc96e9e16d52_delete_user_model.py
+++ migrations/versions/fc96e9e16d52_delete_user_model.py
@@ -23,6 +23,63 @@ def downgrade():
     op.drop_table('workspace')
     op.drop_table('project')
 
+    op.create_table('user',
+                    sa.Column('id', sa.INTEGER(),
+                              autoincrement=True, nullable=False),
+                    sa.Column('name', sa.VARCHAR(),
+                              autoincrement=False, nullable=True),
+                    sa.Column('email', sa.VARCHAR(),
+                              autoincrement=False, nullable=True),
+                    sa.PrimaryKeyConstraint('id', name='user_pkey')
+                    )
+    op.create_table('project',
+                    sa.Column('id', sa.INTEGER(), server_default=sa.text(
+                        'nextval(\'"project_id_seq"\'::regclass)'), autoincrement=True, nullable=False),
+                    sa.Column('name', sa.VARCHAR(),
+                              autoincrement=False, nullable=True),
+                    sa.Column('description', sa.VARCHAR(),
+                              autoincrement=False, nullable=True),
+                    sa.Column('start_date', sa.Date(),
+                              autoincrement=False, nullable=True),
+                    sa.Column('end_date', sa.Timestamp(),
+                              autoincrement=False, nullable=True),
+                    sa.Column('user_id', sa.VARCHAR(),
+                              autoincrement=False, nullable=False),
+                    sa.PrimaryKeyConstraint('id', name='project_pkey')
+                    )
+    op.create_table('workspace',
+                    sa.Column('id', sa.INTEGER(), server_default=sa.text(
+                        'nextval(\'"workspace_id_seq"\'::regclass)'), autoincrement=True, nullable=False),
+                    sa.Column('name', sa.VARCHAR(),
+                              autoincrement=False, nullable=True),
+                    sa.Column('description', sa.VARCHAR(),
+                              autoincrement=False, nullable=True),
+                    sa.Column('price', sa.Float(),
+                              autoincrement=False, nullable=True),
+                    sa.Column('start_date', sa.Date(),
+                              autoincrement=False, nullable=True),
+                    sa.Column('end_date', sa.Timestamp(),
+                              autoincrement=False, nullable=True),
+                    sa.ForeignKeyConstraint(
+                        ('project_id', ), ['project.id'], ondelete="CASCADE"),
+                    sa.PrimaryKeyConstraint('id', name='workspace_pkey')
+                    )
+    op.create_table('workitem',
+                    sa.Column('id', sa.INTEGER(), server_default=sa.text(
+                        'nextval(\'"workitem_id_seq"\'::regclass)'), autoincrement=True, nullable=False),
+                    sa.Column('name', sa.VARCHAR(),
+                              autoincrement=False, nullable=True),
+                    sa.Column('description', sa.VARCHAR(),
+                              autoincrement=False, nullable=True),
+                    sa.Column('duration', sa.Float(),
+                              autoincrement=False, nullable=True),
+                    sa.Column('end_date', sa.Timestamp(),
+                              autoincrement=False, nullable=True),
+                    sa.ForeignKeyConstraint(
+                        ('workspace_id', ),  ['workspace.id'], ondelete="CASCADE"),
+                    sa.PrimaryKeyConstraint('id', name='workitem_pkey')
+                    )
+
     # ### end Alembic commands ###
 
 
@@ -89,19 +146,5 @@ def upgrade():
                         ('workspace_id', ),  ['workspace.id'], ondelete="CASCADE"),
                     sa.PrimaryKeyConstraint('id', name='workitem_pkey')
                     )
-    print(op.create_table('workitem',
-                          sa.Column('id', sa.INTEGER(), server_default=sa.text(
-                              'nextval(\'"workitem_id_seq"\'::regclass)'), autoincrement=True, nullable=False),
-                          sa.Column('name', sa.VARCHAR(),
-                                    autoincrement=False, nullable=True),
-                          sa.Column('description', sa.VARCHAR(),
-                                    autoincrement=False, nullable=True),
-                          sa.Column('duration', sa.Float(),
-                                    autoincrement=False, nullable=True),
-                          sa.Column('end_date', sa.Timestamp(),
-                                    autoincrement=False, nullable=True),
-                          sa.ForeignKeyConstraint(
-                              ('workspace_id', ),  ['workspace.id'], ondelete="CASCADE"),
-                          sa.PrimaryKeyConstraint('id', name='workitem_pkey')
-                          ))
+
     # ### end Alembic commands ###
