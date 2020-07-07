eb1854b78528d8c5eb919bc4e551422f63a1923a
Mike Schertl <schert@hm.edu>
2020-07-07 09:07:44 +0200

    add setup file to run the project locally

---
+++ setup.sh
@@ -0,0 +1,7 @@
+export DATABASE_URL='postgresql://postgres@localhost:5432/casting_agency'
+export AUTH_DOMAIN='https://fsnd2020.eu.auth0.com/'
+export ALGORITHM=['RS256']
+export API_AUDIENCE='freetime'
+export FLASK_APP=app.py
+export FLASK_DEBUG=true
+flask run --reload
